56ec86d89ef8bec55abb75c86ae0be5adc85b55e
陈松国 <csg0804>
2020-04-03 15:43:00 +0800

    update

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/common_h5',
 
     /**
      * Source Maps
